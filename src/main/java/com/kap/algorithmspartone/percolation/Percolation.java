bac5e69e50e34f9deba560cd01e0311376e82535
Kost1s <antonioucostis@gmail.com>
2018-08-12 15:23:05 +0100

    adding isFull method

--- src/main/java/com/kap/algorithmspartone/percolation/Percolation.java
+++ src/main/java/com/kap/algorithmspartone/percolation/Percolation.java
@@ -13,11 +13,11 @@ public class Percolation {
     private static final String SITES_INPUT_SUB_OR_EQUAL_ZERO = "Number of sites provided as an input is invalid." +
                                                                 "Valid input should be an integer biggen than zero";
 
-    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private final WeightedQuickUnionUF weightedQuickUnionUF;
     private boolean[][] openSites;
     private final int gridSideLength;
-    private int virtualTopSite;
-    private int virtualBottomSite;
+    private final int virtualTopSite;
+    private final int virtualBottomSite;
 
     /**
      * Percolation class constructor.
@@ -47,23 +47,39 @@ public class Percolation {
 
     }
 
+    /**
+     * Checks whether a site is full. i.e. whether the site with the provided coordinates is connected with the virtual
+     * top site.
+     *
+     * @param row site row index provided
+     * @param col site column index provided
+     *
+     * @return true if site is full. false otherwise.
+     */
     public boolean isFull(int row, int col) {
-
+        validateIndices(row, col);
+        return weightedQuickUnionUF.connected(virtualTopSite, getUfIndex(row, col));
     }
 
     public int numberOfOpenSites() {
 
     }
 
+    /**
+     * Checks whether the system percolates or not. i.e. whether the virtual top site is connected with the virtual
+     * bottom site.
+     *
+     * @return true if systems percolates. otherwise returns false.
+     */
     public boolean percolates() {
-
+        return weightedQuickUnionUF.connected(virtualTopSite, virtualBottomSite);
     }
 
     /**
      * Validates whether the indices provided are within the valid prescribed range. (1 to n)
      *
-     * @param row row index provided
-     * @param col column index provided
+     * @param row site row index provided
+     * @param col site column index provided
      */
     private void validateIndices(int row, int col) {
         if ((row < 1) || (col < 1) || (row > gridSideLength) || (col > gridSideLength)) {
@@ -88,8 +104,8 @@ public class Percolation {
      * <p>
      * That means that in a 25 by 25 grid. Starting coordinates(1,1) map to 1D coordinates as: (25 * (1 - 1) + 1) = 1
      *
-     * @param row row index provided
-     * @param col column index provided
+     * @param row site row index provided
+     * @param col site column index provided
      */
     private int getUfIndex(int row, int col) {
         return (gridSideLength * (row - 1)) + col;
