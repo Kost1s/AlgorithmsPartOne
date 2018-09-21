6617d34d4187ff1c8df702d4730bbc925a58cb1c
Kost1s <antonioucostis@gmail.com>
2018-09-21 05:54:25 +0100

    slight change to the class in order not to throw exception when reading from std input

--- src/main/java/com/kap/algorithmspartone/stacksandqueues/Permutation.java
+++ src/main/java/com/kap/algorithmspartone/stacksandqueues/Permutation.java
@@ -1,6 +1,5 @@
 package com.kap.algorithmspartone.stacksandqueues;
 
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -12,10 +11,9 @@ public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
 
-        int stringsNo = StdIn.readInt();
-        In in = new In(args[0]);
-        while (!in.isEmpty()) {
-            String item = in.readString();
+        int stringsNo = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
             randomizedQueue.enqueue(item);
         }
 
