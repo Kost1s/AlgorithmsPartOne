527ea87de9a89e9178c3bdf473dc5acb9bd7d1ac
Kost1s <antonioucostis@gmail.com>
2018-10-14 20:26:04 +0100

    starting of commits of brute collinear points

---
+++ src/main/java/com/kap/algorithmspartone/collinearpoints/BruteCollinearPoints.java
@@ -0,0 +1,17 @@
+package com.kap.algorithmspartone.collinearpoints;
+
+/**
+ * @author Konstantinos Antoniou
+ */
+public class BruteCollinearPoints {
+
+    private Point[] points;
+
+    public BruteCollinearPoints(final Point[] points) {
+        this.points = points;
+    }
+
+    private void checkPointsValidity() {
+
+    }
+}
