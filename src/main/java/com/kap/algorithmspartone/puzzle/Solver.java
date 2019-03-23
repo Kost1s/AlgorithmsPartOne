42666db7cadca3410f615794b7be131012041e49
Kost1s <antonioucostis@gmail.com>
2019-03-23 17:01:52 +0000

    adding the code for the solver class

--- src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
+++ src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
@@ -1,10 +1,95 @@
 package com.kap.algorithmspartone.puzzle;
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Stack;
+
 /**
  * @author Konstantinos Antoniou
  */
 public class Solver {
 
-    
+    private Move lastMove;
+
+    public Solver(Board initial) {
+        if(initial == null) {
+            throw new IllegalArgumentException();
+        }
+
+        MinPQ<Move> moves = new MinPQ<>();
+        moves.insert(new Move(initial));
+
+        MinPQ<Move> twinMoves = new MinPQ<>();
+        twinMoves.insert(new Move(initial.twin()));
+
+        while(true) {
+            lastMove = expand(moves);
+            if ((lastMove != null) || (expand(twinMoves) != null)) {
+                return;
+            }
+        }
+    }
+
+    public boolean isSolvable() {
+        return (lastMove != null);
+    }
+
+    public int moves() {
+        return isSolvable() ? lastMove.numMoves : -1;
+    }
+
+    public Iterable<Board> solution() {
+        if (!isSolvable()) {
+            return null;
+        }
+
+        Stack<Board> moves = new Stack<>();
+        while(lastMove != null) {
+            moves.push(lastMove.board);
+            lastMove = lastMove.previous;
+        }
+
+        return moves;
+    }
+
+    private Move expand(MinPQ<Move> moves) {
+        if(moves.isEmpty()) {
+            return null;
+        }
+
+        Move idealMove = moves.delMin();
+
+        if (idealMove.board.isGoal()) {
+            return idealMove;
+        }
+        for (Board neighbor : idealMove.board.neighbors()) {
+            if ((idealMove.previous == null) || !neighbor.equals(idealMove.previous.board)) {
+                moves.insert(new Move(neighbor, idealMove));
+            }
+        }
+        return null;
+    }
+
+    private class Move implements Comparable<Move> {
+        private Move previous;
+        private final Board board;
+        private int numMoves = 0;
+        private int priority;
+
+        public Move(Board board) {
+            this.board = board;
+        }
+
+        public Move(Board board, Move previous) {
+            this.board = board;
+            this.previous = previous;
+            numMoves = previous.numMoves + 1;
+            priority = board.manhattan() + numMoves;
+        }
+
+        public int compareTo(Move move) {
+            return (priority - move.priority);
+        }
+    }
 
 }
