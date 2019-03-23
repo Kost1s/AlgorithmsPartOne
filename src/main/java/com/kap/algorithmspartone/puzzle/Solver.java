479170f544c1eebcfff6990c94d8d4b28cb6d251
Kost1s <antonioucostis@gmail.com>
2019-03-23 17:14:28 +0000

    adding main

--- src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
+++ src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
@@ -1,6 +1,6 @@
 package com.kap.algorithmspartone.puzzle;
 
-import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.*;
 
 import java.util.Stack;
 
@@ -89,6 +89,30 @@ public class Solver {
 
         public int compareTo(Move move) {
             return (priority - move.priority);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
         }
     }
 
