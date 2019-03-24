f974945f58f75dfa576efb9788012221676b8e2a
Kost1s <antonioucostis@gmail.com>
2019-03-24 22:09:59 +0000

    importing just the util collection that has to be used

--- src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
+++ src/main/java/com/kap/algorithmspartone/puzzle/Solver.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.*;
+import java.util.ArrayDeque;
 
 /**
  * @author Konstantinos Antoniou
